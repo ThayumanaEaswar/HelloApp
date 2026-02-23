cd054bdcf0c7b9e1a0d2a01b0e8674c567317f74
ThayumanaEaswar <tn5943@srmist.edu.in>
2026-02-23 20:27:44 +0530

    feat: UC2 complete - 7-line stylized banner v2.0

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,16 @@
 /**
- * @author YourName
- * @version 1.0
+ * @author ThayumanaEaswar
+ * @version 2.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");   
+        // UC2: Displaying "OOPS" in a large banner using 7 individual print statements
+        System.out.println("  **** **** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("  **** **** * **** ");
     }
 }
